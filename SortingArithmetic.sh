dc9f6151a43216e39c22cc0a1f21f3a994ebe159
rojajullapalli <rojajules@gmail.com>
2021-07-22 11:33:20 +0530

    take inputs a b c

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -x
 echo "Welcome to sorting arthmetic computation"
+read -p "enter the value of a:" a
+read -p "enter the value of b:" b
+read -p "enter the value of c:" c
+echo "$a $b $c"
