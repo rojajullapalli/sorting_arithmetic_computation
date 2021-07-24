aba866ccb1b8453f19cafd8c591c780819924041
rojajullapalli <rojajules@gmail.com>
2021-07-24 13:59:49 +0530

    adding a * b + c

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -4,5 +4,5 @@ read -p "enter the value of a:" a
 read -p "enter the value of b:" b
 read -p "enter the value of c:" c
 echo "$a $b $c"
-uc2=$((a+($b*$c)))
-
+uc2=$(($a+($b*$c)))
+uc3=$((($a*$b)+$c))
