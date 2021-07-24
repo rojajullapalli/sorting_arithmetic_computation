08f8e76488c27b2d73d64eb0a273db71fc0a9c94
rojajullapalli <rojajules@gmail.com>
2021-07-24 15:01:08 +0530

    Store the results in

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -8,3 +8,8 @@ uc2=$(($a+($b*$c)))
 uc3=$((($a*$b)+$c))
 uc4=$(($c +($a/$b)))
 uc5=$((($a%$b)+$c))
+declare -A arithmetic
+arithmetic[1]="$uc2"
+arithmetic[2]="$uc3"
+arithmetic[3]="$uc4"
+arithmetic[4]="$uc5"
